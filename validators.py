4e27dbff814d08230b0f3a7d2be599dfbf46f3f1
RaisonCollab <danil.zinovev.16@mail.ru>
2023-12-19 21:35:45 +0300

    version 1.0.2 connect new window

--- validators.py
+++ validators.py
@@ -2,8 +2,14 @@ import typing
 
 
 class FieldValidator:
-    def valid_fields(self):
+    def valid_fields(self) -> bool:
         ...
 
     def clear_fields(self):
         ...
+
+    def count_words(self, st: str) -> int:
+        return len(st.split())
+
+    def is_empty(self, st: str) -> bool:
+        return not bool(len(st))
--- windows.py
+++ windows.py
@@ -1,4 +1,5 @@
 import datetime
+import re
 from pprint import pprint
 
 from PyQt6 import uic
@@ -9,7 +10,6 @@ from database_controller import DatabaseController
 from messages_handler import MessageHandler
 from utils import Util
 from validators import FieldValidator
-
 
 db = DatabaseController('data.db')
 
@@ -62,7 +62,8 @@ class RegWindow(QMainWindow, MessageHandler, FieldValidator):
         check_employee = db.check_employee_by_card_number(card_number)
 
         if check_employee:
-            return self.show_message(QMessageBox.Icon.Critical, 'Ошибка', 'Пользователь с таким номером пропуска существует')
+            return self.show_message(QMessageBox.Icon.Critical, 'Ошибка',
+                                     'Пользователь с таким номером пропуска существует')
 
         db.add_employee((card_number, f_name, s_name, l_name, birthday, jobtitle, p_number, mail))
 
@@ -80,22 +81,64 @@ class RegWindow(QMainWindow, MessageHandler, FieldValidator):
         first_name_text = self.first_name_field.text().strip()
         second_name_text = self.second_name_field.text().strip()
         last_name_text = self.last_name_field.text().strip()
-        birthday_text = self.birthday_field.text().strip()
+        birthday_text = self.birthday_field.text().strip().replace('.', '/')
         jobtitle_text = self.jobtitle_field.text().strip()
-        phone_number_text = self.phone_number_field.text().strip()
+        phone_number_text = self.phone_number_field.text().strip().replace('+', '')
         mail_text = self.mail_field.text().strip()
 
-        if (not len(first_name_text) or len(first_name_text.split()) > 1) or \
-                (not len(second_name_text) or len(second_name_text.split()) > 1) or \
-                (not len(last_name_text) or len(last_name_text.split()) > 1) or \
-                (not len(birthday_text) or len(birthday_text.split()) > 1) or \
-                (not len(jobtitle_text) or len(jobtitle_text.split()) > 1) or \
-                (not len(phone_number_text) or len(phone_number_text.split()) > 1) or \
-                (not len(mail_text) or len(mail_text.split()) > 1) or \
-                (len(card_number_text.split()) > 1 or not card_number_text.isdigit()):
-            return False
+        check_card_number = (not self.is_empty(card_number_text) and self.count_words(
+            card_number_text) == 1 and card_number_text.isdigit())
+        check_f_name = (not self.is_empty(first_name_text) and self.count_words(first_name_text) == 1)
+        check_s_name = (not self.is_empty(second_name_text) and self.count_words(second_name_text) == 1)
+        check_l_name = (not self.is_empty(last_name_text) and self.count_words(last_name_text) == 1)
+        check_birthday = False if re.match(r'(0?[1-9]|[12][0-9]|3[01]).(0?[1-9]|1[012]).((19|20)\d\d)',
+                                           birthday_text) is None or self.count_words(birthday_text) > 1 else True
+        check_jobtitle = (not self.is_empty(jobtitle_text) and self.count_words(jobtitle_text) == 1)
+        check_phone_number = False if re.match(r'((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}',
+                                               phone_number_text) is None or self.count_words(
+            phone_number_text) > 1 or len(phone_number_text) != 11 or not phone_number_text.isdigit() else True
+        check_mail = False if re.match(r'([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)',
+                                       mail_text) is None or self.count_words(mail_text) > 1 else True
 
-        return True
+        return True if (
+                    check_card_number and check_f_name and check_s_name and check_l_name and check_birthday and check_jobtitle and check_phone_number and check_mail) else False
+
+
+class TempCardWindow(QMainWindow, MessageHandler, FieldValidator):
+    def __init__(self):
+        super().__init__()
+
+        uic.loadUi('temp_card_window.ui', self)
+
+        self.first_name_field: QLineEdit = self.findChild(QLineEdit, 'first_name')
+        self.second_name_field: QLineEdit = self.findChild(QLineEdit, 'second_name')
+        self.card_number_field: QLineEdit = self.findChild(QLineEdit, 'card_number')
+
+        self.access_button: QPushButton = self.findChild(QPushButton, 'access_button')
+
+        self.set_events()
+        self.set_card_number()
+
+    def set_events(self):
+        self.access_button.clicked.connect(self.issue_temp_card)
+
+    def set_card_number(self):
+        all_cards_numbers = [row[0] for row in db.get_all_data_employee_cards()]
+        card_number = Util.generate_random_card_number([1000, 1_000_000])
+
+        if card_number in all_cards_numbers:
+            return self.set_card_number()
+
+        self.card_number_field.setText(str(card_number))
+
+    def issue_temp_card(self):
+        print('click')
+        self.close()
+
+    def clear_fields(self):
+        self.first_name_field.clear()
+        self.second_name_field.clear()
+        self.card_number_field.clear()
 
 
 class MainWindow(QMainWindow, MessageHandler, FieldValidator):
@@ -107,17 +150,20 @@ class MainWindow(QMainWindow, MessageHandler, FieldValidator):
         self.reg_button: QPushButton = self.findChild(QPushButton, 'RegButton')
         self.access_employee_button: QPushButton = self.findChild(QPushButton, 'access_employee_button')
         self.out_employee_button: QPushButton = self.findChild(QPushButton, 'out_employee_button')
+        self.issue_temp_card_button: QPushButton = self.findChild(QPushButton, 'temp_card_button')
 
         self.card_number_field: QLineEdit = self.findChild(QLineEdit, 'card_number')
 
         self.set_events()
 
         self.reg_window = None
+        self.temp_card_window = None
 
     def set_events(self):
         self.reg_button.clicked.connect(self.show_reg_window)
         self.access_employee_button.clicked.connect(self.access_employee)
         self.out_employee_button.clicked.connect(self.out_employee)
+        self.issue_temp_card_button.clicked.connect(self.show_temp_card_window)
 
     def access_employee(self):
         if not self.valid_fields():
@@ -129,9 +175,8 @@ class MainWindow(QMainWindow, MessageHandler, FieldValidator):
         if not check_employee:
             return self.show_message(QMessageBox.Icon.Critical, 'Ошибка', 'Данного пользователя не существует')
 
-        # todo Update field with time in DB
         employee_data = db.get_employee_by_card_number(card_number)
-        date_time_in = datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+        date_time_in = datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
 
         db.add_employee_visiting_time_in(card_number, date_time_in)
 
@@ -150,13 +195,14 @@ class MainWindow(QMainWindow, MessageHandler, FieldValidator):
         self.clear_fields()
 
     def out_employee(self):
-        date_time_out = datetime.datetime.now().strftime('%d.%m.%Y %H:%M:%S')
+        date_time_out = datetime.datetime.now().strftime('%d/%m/%Y %H:%M:%S')
         card_number = int(self.card_number_field.text())
 
         if not db.check_employee_by_card_number(card_number):
             return self.show_message(QMessageBox.Icon.Critical, 'Ошибка', 'Данного пользователя не существует')
 
-        employee_visiting = db.get_employee_visiting(card_number)[-1] if db.check_employee_in_visitors(card_number) else [False]  # связано с проверкой в строке 151, чтобы не переписывать код, сделал костыль)
+        employee_visiting = db.get_employee_visiting(card_number)[-1] if db.check_employee_in_visitors(
+            card_number) else [False]  # связано с проверкой ниже, чтобы не переписывать код, сделал костыль)
 
         if employee_visiting[-1] is not None:
             return self.show_message(QMessageBox.Icon.Critical, 'Ошибка', 'Данный сотрудник не заходил на территорию')
@@ -166,8 +212,6 @@ class MainWindow(QMainWindow, MessageHandler, FieldValidator):
         employee_data = db.get_employee_by_card_number(card_number)
         employee_visiting = db.get_employee_visiting(card_number)[-1]
         time_diff = Util.calc_time_different(*employee_visiting[1:])
-
-        # todo Доработать базу данных в коде
 
         report_message = (f'Номер пропуска: {employee_data[0]}\n'
                           f'Имя: {employee_data[1]}\n'
@@ -197,3 +241,7 @@ class MainWindow(QMainWindow, MessageHandler, FieldValidator):
     def show_reg_window(self):
         self.reg_window = RegWindow()
         self.reg_window.show()
+
+    def show_temp_card_window(self):
+        self.temp_card_window = TempCardWindow()
+        self.temp_card_window.show()
